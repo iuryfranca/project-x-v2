f1601cafae5a3da2741e62c79b52707e66477c6d
Iury Franca <iurygfranca@gmail.com>
2023-05-04 17:15:40 -0400

    feet: new login page exemple ✨

--- config/site.ts
+++ config/site.ts
@@ -1,17 +1,16 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Project X v2",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: 'Project X v2',
+  description: 'A simple and functional marketplace to have in your portfolio',
   mainNav: [
     {
-      title: "Home",
-      href: "/",
+      title: 'Home',
+      href: '/',
     },
   ],
   links: {
-    linkedin: "https://www.linkedin.com/in/iury-franca-37873318b/",
-    github: "https://github.com/iuryfranca/",
+    linkedin: 'https://www.linkedin.com/in/iury-franca-37873318b/',
+    github: 'https://github.com/iuryfranca/',
   },
 }

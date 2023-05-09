344b69e92ebdc894492298fcc006ce0373cd1362
Iury Franca <iurygfranca@gmail.com>
2023-05-09 11:53:17 -0400

    teste: testing build [ignore this commit] 🚨

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { UserButton, useUser } from '@clerk/nextjs'
 
 export default function DashboardPage() {
   const { isLoaded, isSignedIn, user } = useUser()
+  console.log('testing build [ignore this commit]')
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <h1>Hello World</h1>

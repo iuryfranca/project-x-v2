6ecfab2a73657124bf5b4e310bbdfaf643decc57
iuryfranca <iurygfranca@gmail.com>
2023-05-10 00:33:09 -0400

    teste: testing build [ignore this commit] 5 🚨

--- env.mjs
+++ env.mjs
@@ -1,11 +1,8 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    VERCEL: z.string().min(1),
-    VERCEL_ENV: z.string().min(1),
-    TURBO_REMOTE_ONLY: z.string().min(1),
     NX_DAEMON: z.string().min(1),
     KV_URL: z.string().min(1),
     KV_REST_API_URL: z.string().min(1),
@@ -17,17 +14,14 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    VERCEL: process.env.VERCEL,
-    VERCEL_ENV: process.env.VERCEL_ENV,
-    TURBO_REMOTE_ONLY: process.env.TURBO_REMOTE_ONLY,
-
     NX_DAEMON: process.env.NX_DAEMON,
     KV_URL: process.env.KV_URL,
     KV_REST_API_URL: process.env.KV_REST_API_URL,
     KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
     KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
 
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
   },
 })
